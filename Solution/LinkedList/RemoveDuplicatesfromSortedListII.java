f355e3a6fa9e7c4eb2b51b651d9b0a779bdb6a95
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-11 01:00:03 +0530

    Update

--- Solution/LinkedList/RemoveDuplicatesfromSortedListII.java
+++ Solution/LinkedList/RemoveDuplicatesfromSortedListII.java
@@ -1,18 +1,21 @@
+/*Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+*/
+
 package Solution.LinkedList;
 
-public class RemoveDuplicatesfromSortedListII {
+ class Solution {
 
-    public RemoveDuplicatesfromSortedListII deleteDuplicates(RemoveDuplicatesfromSortedListII head) {
+    int val;
+    Solution next;
+    Solution() {};
+    Solution(int val) { this.val = val; }
+    Solution(int val, Solution next) { this.val = val; this.next = next; }
 
-        int val;
-        RemoveDuplicatesfromSortedListII next;
-        RemoveDuplicatesfromSortedListII() {}
-        RemoveDuplicatesfromSortedListII(int val) { this.val = val; }
-        RemoveDuplicatesfromSortedListII(int val, RemoveDuplicatesfromSortedListII next) { this.val = val; this.next = next; }
-  
+    public static Solution deleteDuplicates(Solution head) {
         //use two pointers, slow - track the node before the dup nodes, 
         // fast - to find the last node of dups.
-        RemoveDuplicatesfromSortedListII dummy = new ListNode(0), fast = head, slow = dummy;
+        Solution dummy = new Solution(0), fast = head, slow = dummy;
         slow.next = fast;
         while(fast != null) {
             while (fast.next != null && fast.val == fast.next.val) {
@@ -29,5 +32,18 @@ public class RemoveDuplicatesfromSortedListII {
         }
         return dummy.next;
     }
-    
+
+    public static void main(String args[]) {
+        Solution head = new Solution(1);
+        head.next = new Solution(2);
+        head.next = new Solution(0);
+        head.next = new Solution(-4);
+
+        Solution res = deleteDuplicates(head);
+        while(res != null) {
+            System.out.print(res.val + " ");
+            res = res.next;
+          }
+          System.out.println();
+    }    
 }
