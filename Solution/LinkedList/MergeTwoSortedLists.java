42aba533519f696c6f90a8b40b9468a5327e7fcd
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-11 21:47:43 +0530

    Update MergeTwoSortedLists.java

--- Solution/LinkedList/MergeTwoSortedLists.java
+++ Solution/LinkedList/MergeTwoSortedLists.java
@@ -6,33 +6,36 @@ https://leetcode.com/problems/merge-two-sorted-lists/
 
 package Solution.LinkedList;
  
-class Solution {
+class MergeTwoSortedLists {
+
     int val;
-    Solution next;
-    Solution() {}
-    Solution(int val) { this.val = val; }
-    Solution(int val, Solution next) { this.val = val; this.next = next; }
-    public static Solution mergeTwoLists(Solution l1, Solution l2){
-		if(l1 == null) return l2;
-		if(l2 == null) return l1;
-		if(l1.val < l2.val){
-			l1.next = mergeTwoLists(l1.next, l2);
-			return l1;
-		} else{
-			l2.next = mergeTwoLists(l1, l2.next);
-			return l2;
-		}
+    MergeTwoSortedLists next;
+    MergeTwoSortedLists() {}
+    MergeTwoSortedLists(int val) { this.val = val; }
+    MergeTwoSortedLists(int val, MergeTwoSortedLists next) { this.val = val; this.next = next; }
+
+    public static MergeTwoSortedLists mergeTwoLists(MergeTwoSortedLists l1, MergeTwoSortedLists l2){
+		    if(l1 == null) return l2;
+		    if(l2 == null) return l1;
+		    if(l1.val < l2.val){
+			      l1.next = mergeTwoLists(l1.next, l2);
+			      return l1;
+		    } 
+        else{
+			      l2.next = mergeTwoLists(l1, l2.next);
+			      return l2;
+		    }
     }
     
     public static void main(String args[]) {
-        Solution l1 = new Solution(1);
-        l1.next = new Solution(2);
-        l1.next.next = new Solution(4);
-        Solution l2 = new Solution(1);
-        l1.next = new Solution(3);
-        l1.next.next = new Solution(4);
+        MergeTwoSortedLists l1 = new MergeTwoSortedLists(1);
+        l1.next = new MergeTwoSortedLists(2);
+        l1.next.next = new MergeTwoSortedLists(4);
+        MergeTwoSortedLists l2 = new MergeTwoSortedLists(1);
+        l2.next = new MergeTwoSortedLists(5);
+        l2.next.next = new MergeTwoSortedLists(8);
 
-        Solution res = mergeTwoLists(l1, l2);
+        MergeTwoSortedLists res = mergeTwoLists(l1, l2);
         while(res != null) {
             System.out.print(res.val + " ");
             res = res.next;
