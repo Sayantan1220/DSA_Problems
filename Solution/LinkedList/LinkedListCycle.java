48f8571e1fde38b9cafc0056d046e5cf6ac869a5
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-11 21:38:23 +0530

    Update LinkedListCycle.java

--- Solution/LinkedList/LinkedListCycle.java
+++ Solution/LinkedList/LinkedListCycle.java
@@ -1,3 +1,9 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+https://leetcode.com/problems/linked-list-cycle/
+*/
+
 package Solution.LinkedList;
 
 public class LinkedListCycle {
@@ -16,16 +22,18 @@ public class LinkedListCycle {
         while(runner.next!=null && runner.next.next!=null) {
             walker = walker.next;
             runner = runner.next.next;
-            if(walker==runner) return true;
+            if(walker==runner) {
+                return true;
+            }            
         }
         return false;
     }
 
     public static void main(String args[]) {
-        LinkedListCycle head = new LinkedListCycle(1);
+        LinkedListCycle head = new LinkedListCycle(3);
         head.next = new LinkedListCycle(2);
-        head.next = new LinkedListCycle(0);
-        head.next = new LinkedListCycle(-4);
+        head.next.next = new LinkedListCycle(1);
+        head.next.next.next = new LinkedListCycle(-4);
 
         boolean res = hasCycle(head);
         System.out.println(res);
