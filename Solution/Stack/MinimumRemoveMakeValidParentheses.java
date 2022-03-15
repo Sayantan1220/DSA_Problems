ef986c3f660a387191163cc9fa15b6796f3e0688
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-15 21:14:46 +0530

    Create MinimumRemoveMakeValidParentheses.java

---
+++ Solution/Stack/MinimumRemoveMakeValidParentheses.java
@@ -0,0 +1,29 @@
+/*Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+Formally, a parentheses string is valid if and only if:
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+*/
+
+package Solution.Stack;
+import java.util.Stack;
+
+public class MinimumRemoveMakeValidParentheses {
+
+public String minRemoveToMakeValid(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < sb.length(); ++i) {
+        if (sb.charAt(i) == '(') st.add(i);
+        if (sb.charAt(i) == ')') {
+            if (!st.empty()) st.pop();
+        else sb.setCharAt(i, '*');
+        }
+    }
+    while (!st.empty())
+        sb.setCharAt(st.pop(), '*');
+    return sb.toString().replaceAll("\\*", "");
+    }
+}
