3b938cc39a061eea7388e14cf37843f464cbca5c
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-16 20:48:16 +0530

    Update ValidateStackSequences.java

--- Solution/Stack/ValidateStackSequences.java
+++ Solution/Stack/ValidateStackSequences.java
@@ -1,9 +1,13 @@
+/*Given two integer arrays pushed and popped each with distinct values, 
+return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+https://leetcode.com/problems/validate-stack-sequences/
+*/
 
 package Solution.Stack;
 import java.util.Stack;
 
 public class ValidateStackSequences {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack = new Stack<>();
         int i = 0;
         for (int x : pushed) {
@@ -14,5 +18,12 @@ public class ValidateStackSequences {
             }
         }
         return stack.empty();
-    }   
+    }
+      
+    public static void main(String args[]) {
+        int[] push = {1,2,3,4,5};
+        int[] pop = {4,3,5,1,2};
+        boolean res = validateStackSequences(push, pop);
+        System.out.println(res);
+    }
 }
