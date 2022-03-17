0b5271904e462c502801c5be8a7900bfc3a88f04
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-17 21:04:40 +0530

    Update ScoreofParentheses.java

--- Solution/Stack/ScoreofParentheses.java
+++ Solution/Stack/ScoreofParentheses.java
@@ -1,11 +1,19 @@
+/*Given a balanced parentheses string s, return the score of the string.
+The score of a balanced parentheses string is based on the following rule:
+"()" has score 1.
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+https://leetcode.com/problems/score-of-parentheses/
+*/
+
 package Solution.Stack;
 import java.util.Stack;
 
 public class ScoreofParentheses {
-    public int scoreOfParentheses(String S) {
+    public static int scoreOfParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
         int cur = 0;
-        for (char c : S.toCharArray()) {
+        for (char c : s.toCharArray()) {
             if (c == '(') {
                 stack.push(cur);
                 cur = 0;
@@ -15,4 +23,10 @@ public class ScoreofParentheses {
         }
         return cur;
     }    
+
+    public static void main(String args[]) {
+        String s = "(())";
+        int res = scoreOfParentheses(s);
+        System.out.println(res);
+    }
 }
