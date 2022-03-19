f4822f9b8d7630b8d80736359a869841c2a46140
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-19 12:35:50 +0530

    Update MaximumFrequencyStack.java

--- Solution/Stack/MaximumFrequencyStack.java
+++ Solution/Stack/MaximumFrequencyStack.java
@@ -1,3 +1,12 @@
+/*Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+Implement the FreqStack class:
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+https://leetcode.com/problems/maximum-frequency-stack/
+*/
+
 package Solution.Stack;
 import java.util.*;
 
@@ -20,5 +29,22 @@ public class MaximumFrequencyStack {
         if (m.get(maxfreq).size() == 0) maxfreq--;
         return x;
     }
-    
+
+    public static void main(String args[]) {
+        MaximumFrequencyStack obj = new MaximumFrequencyStack();
+        obj.push(5);
+        obj.push(7);
+        obj.push(5);
+        obj.push(7);
+        obj.push(4);
+        obj.push(5);
+        int one = obj.pop();
+        System.out.println(one);
+        int two = obj.pop();
+        System.out.println(two);
+        int three = obj.pop();
+        System.out.println(three);
+        int four = obj.pop();
+        System.out.println(four);
+    }   
 }
