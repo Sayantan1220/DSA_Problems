988204ce930fe08d833b3c6dc6834ca6ebf868a1
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-24 21:17:36 +0530

    Update BoatsToSavePeople.java

--- Solution/Array/BoatsToSavePeople.java
+++ Solution/Array/BoatsToSavePeople.java
@@ -1,8 +1,14 @@
+/*You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. 
+Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+Return the minimum number of boats to carry every given person.
+https://leetcode.com/problems/boats-to-save-people/
+*/
+
 package Solution.Array;
 import java.util.*;
 
 public class BoatsToSavePeople {
-    public int numRescueBoats(int[] people, int limit) {
+    public static int numRescueBoats(int[] people, int limit) {
         int boatCount = 0;
         Arrays.sort(people);
         
@@ -23,5 +29,11 @@ public class BoatsToSavePeople {
         }
         return boatCount;
     }
-    
+
+    public static void main(String args[]) {
+        int people[] = {3,2,2,1};
+        int limit = 3;
+        int res = numRescueBoats(people, limit);
+        System.out.println(res);
+    }   
 }
