e068b58aba1ad58eacefa95b86fc66908194869a
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-25 21:14:56 +0530

    Update TwoCityScheduling.java

--- Solution/Array/TwoCityScheduling.java
+++ Solution/Array/TwoCityScheduling.java
@@ -1,8 +1,14 @@
+/*A company is planning to interview 2n people. 
+Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.
+Return the minimum cost to fly every person to a city such that exactly n people arrive in each city.
+https://leetcode.com/problems/two-city-scheduling/
+*/
+
 package Solution.Array;
 import java.util.*;
 
 public class TwoCityScheduling {
-    public int twoCitySchedCost(int[][] costs) {
+    public static int twoCitySchedCost(int[][] costs) {
         Arrays.sort(costs, (a, b) -> {
             return (a[0] - a[1]) - (b[0] - b[1]);
         });
@@ -16,5 +22,10 @@ public class TwoCityScheduling {
         }
         return price;
     }
-    
+
+    public static void main(String args[]) {
+        int cost[][] = {{10,20},{30,200},{400,50},{30,20}};
+        int res = twoCitySchedCost(cost);
+        System.out.println(res);
+    }  
 }
