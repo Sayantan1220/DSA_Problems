b3d5446f3e064ecaf44994eba5fc1144aada8bdd
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-27 00:18:34 +0530

    Update BinarySearch.java

--- Solution/SearchingSorting/BinarySearch.java
+++ Solution/SearchingSorting/BinarySearch.java
@@ -1,7 +1,14 @@
+/*Given an array of integers nums which is sorted in ascending order, 
+and an integer target, write a function to search target in nums. If target exists, 
+then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+https://leetcode.com/problems/binary-search/
+*/
+
 package Solution.SearchingSorting;
 
 public class BinarySearch {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
         
@@ -19,4 +26,10 @@ public class BinarySearch {
         return -1;
     }
     
+    public static void main(String args[]) {
+        int nums[] = {-1,0,3,5,9,12};
+        int target = 9;
+        int res = search(nums, target);
+        System.out.println(res);
+    }
 }
