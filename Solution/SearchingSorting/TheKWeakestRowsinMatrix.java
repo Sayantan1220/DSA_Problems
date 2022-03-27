c0c02b97c3666fe6705f6127a071bbaed66fa182
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-27 23:50:59 +0530

    Update TheKWeakestRowsinMatrix.java

--- Solution/SearchingSorting/TheKWeakestRowsinMatrix.java
+++ Solution/SearchingSorting/TheKWeakestRowsinMatrix.java
@@ -1,8 +1,18 @@
+/*You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). 
+The soldiers are positioned in front of the civilians. 
+That is, all the 1's will appear to the left of all the 0's in each row.
+A row i is weaker than a row j if one of the following is true:
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+*/
+
 package Solution.SearchingSorting;
 import java.util.*;
 
 public class TheKWeakestRowsinMatrix {
-    public int[] kWeakestRows(int[][] mat, int k) {
+    public static int[] kWeakestRows(int[][] mat, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
         int[] ans = new int[k];
         
@@ -18,7 +28,7 @@ public class TheKWeakestRowsinMatrix {
         return ans;
     }
     
-    private int numOnes(int[] row) {
+    private static int numOnes(int[] row) {
         int lo = 0;
         int hi = row.length;
         
@@ -32,5 +42,13 @@ public class TheKWeakestRowsinMatrix {
         }   
         return lo;
     }
-    
+
+    public static void main(String args[]) {
+        int mat[][] = {{1,1,0,0,0}, {1,1,1,1,0}, {1,0,0,0,0}, {1,1,0,0,0}, {1,1,1,1,1}};
+        int k = 3;
+        int res[] = kWeakestRows(mat, k);
+        for (int i = 0; i < res.length; i++){
+            System.out.println(res[i]);
+        }
+    }    
 }
