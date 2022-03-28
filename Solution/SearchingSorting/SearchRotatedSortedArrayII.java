2d991ef8c865ad27398ad20cbab7b45d3db5b844
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-29 01:12:55 +0530

    Update SearchRotatedSortedArrayII.java

--- Solution/SearchingSorting/SearchRotatedSortedArrayII.java
+++ Solution/SearchingSorting/SearchRotatedSortedArrayII.java
@@ -1,7 +1,14 @@
+/*There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+You must decrease the overall operation steps as much as possible.
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+*/
+
 package Solution.SearchingSorting;
 
 public class SearchRotatedSortedArrayII {
-    public boolean search(int[] nums, int target) {
+    public static boolean search(int[] nums, int target) {
         int start = 0, end = nums.length - 1, mid = -1;
         while(start <= end) {
             mid = (start + end) / 2;
@@ -31,5 +38,12 @@ public class SearchRotatedSortedArrayII {
         }
         
         return false;
-    }    
+    } 
+    
+    public static void main(String args[]) {
+        int nums[] = {2,5,6,0,0,1,2};
+        int target = 3;
+        boolean res = search(nums, target);
+        System.out.println(res);
+    }
 }
