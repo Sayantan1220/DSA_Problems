0dc8f4e342e42c531b2acd9228c20a8cf511dbb7
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-03-30 00:37:25 +0530

    Create FindDuplicateNumber.java

---
+++ Solution/Array/FindDuplicateNumber.java
@@ -0,0 +1,17 @@
+package Solution.Array;
+
+public class FindDuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int[] cnt = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            cnt[nums[i]]++;
+            if (cnt[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+    
+}
