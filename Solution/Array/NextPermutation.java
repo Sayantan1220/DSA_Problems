c6b690e3e069abc97397bac5fcc8bfdf6161a5c2
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-04-04 01:33:51 +0530

    Update NextPermutation.java

--- Solution/Array/NextPermutation.java
+++ Solution/Array/NextPermutation.java
@@ -1,7 +1,20 @@
+/*A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. 
+If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+The replacement must be in place and use only constant extra memory.
+https://leetcode.com/problems/next-permutation/
+*/
+
 package Solution.Array;
 
 public class NextPermutation {
-/*0*/   public void nextPermutation(int[] nums) {
+/*0*/   public static void nextPermutation(int[] nums) {
         // pivot is the element just before the non-increasing (weakly decreasing) suffix
 /*2*/   int pivot = indexOfLastPeak(nums) - 1;
         // paritions nums into [prefix pivot suffix]
@@ -18,7 +31,7 @@ public class NextPermutation {
      * In case there are multiple equal peaks, return the first of those.
      * @return first index of last peak
      */
-/*1*/ int indexOfLastPeak(int[] nums) {
+static /*1*/ int indexOfLastPeak(int[] nums) {
         for (int i = nums.length - 1; 0 < i; --i) {
             if (nums[i - 1] < nums[i]) return i;
         }
@@ -26,7 +39,7 @@ public class NextPermutation {
     }
 
     /** @return last index where the {@code num > threshold} or -1 if not found */
-/*3*/ int lastIndexOfGreater(int[] nums, int threshold) {
+static /*3*/ int lastIndexOfGreater(int[] nums, int threshold) {
         for (int i = nums.length - 1; 0 <= i; --i) {
             if (threshold < nums[i]) return i;
         }
@@ -34,17 +47,22 @@ public class NextPermutation {
     }
 
     /** Reverse numbers starting from an index till the end. */
-    void reverseSuffix(int[] nums, int start) {
+    static void reverseSuffix(int[] nums, int start) {
         int end = nums.length - 1;
         while (start < end) {
             swap(nums, start++, end--);
         }
     }
     
-    void swap(int[] nums, int i, int j) {
+    static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    
+
+    public static void main(String args[]) {
+        int nums[] = {3,1,3,4,2};
+        nextPermutation(nums);
+        System.out.println();
+    } 
 }
