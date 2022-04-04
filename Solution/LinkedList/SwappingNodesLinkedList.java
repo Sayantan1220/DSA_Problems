f673e67a67fb2a6eb1ec418d5c9d94343c7e0b90
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-04-05 02:02:01 +0530

    Update SwappingNodesLinkedList.java

--- Solution/LinkedList/SwappingNodesLinkedList.java
+++ Solution/LinkedList/SwappingNodesLinkedList.java
@@ -1,3 +1,8 @@
+/*You are given the head of a linked list, and an integer k.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+*/
+
 package Solution.LinkedList;
 
 public class SwappingNodesLinkedList {
@@ -8,7 +13,7 @@ public class SwappingNodesLinkedList {
     SwappingNodesLinkedList(int val) { this.val = val; }
     SwappingNodesLinkedList(int val, SwappingNodesLinkedList next) { this.val = val; this.next = next; }
 
-    public SwappingNodesLinkedList swapNodes(SwappingNodesLinkedList head, int k) {
+    public static SwappingNodesLinkedList swapNodes(SwappingNodesLinkedList head, int k) {
         SwappingNodesLinkedList n1 = null, n2 = null;
         for (var p = head; p != null; p = p.next) {
             n2 = n2 == null ? null : n2.next;
@@ -22,5 +27,20 @@ public class SwappingNodesLinkedList {
         n2.val = tmp;
         return head;
     }
-    
+
+    public static void main(String args[]) {
+        SwappingNodesLinkedList l1 = new SwappingNodesLinkedList(1);
+        l1.next = new SwappingNodesLinkedList(2);
+        l1.next.next = new SwappingNodesLinkedList(3);
+        l1.next.next.next = new SwappingNodesLinkedList(4);
+        l1.next.next.next.next = new SwappingNodesLinkedList(5);
+        int k = 2;
+
+        SwappingNodesLinkedList res = swapNodes(l1, k);
+        while(res != null) {
+            System.out.print(res.val + " ");
+            res = res.next;
+          }
+          System.out.println();
+    }  
 }
