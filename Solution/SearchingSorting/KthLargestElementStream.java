26cfc8d4ed1af3b450a4f14c82ba014e5b73efb7
SAYANTAN DAS <sayantan.das208@gmail.com>
2022-04-08 10:36:47 +0530

    Update KthLargestElementStream.java

--- Solution/SearchingSorting/KthLargestElementStream.java
+++ Solution/SearchingSorting/KthLargestElementStream.java
@@ -1,5 +1,12 @@
-package Solution.SearchingSorting;
+/*Design a class to find the kth largest element in a stream. 
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Implement KthLargest class:
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+https://leetcode.com/problems/kth-largest-element-in-a-stream/
+*/
 
+package Solution.SearchingSorting;
 import java.util.*;
 
 public class KthLargestElementStream {
@@ -22,5 +29,13 @@ public class KthLargestElementStream {
         }        
         return minHeap.peek();
     }
-    
+
+    public static void main(String args[]) {
+        int nums[] = {4, 5, 8, 2};
+        int k = 3;
+        KthLargestElementStream kthLargest = new KthLargestElementStream();
+        kthLargest.KthLargest(k, nums);
+        int res = kthLargest.add(3);
+        System.out.println(res);
+    } 
 }
