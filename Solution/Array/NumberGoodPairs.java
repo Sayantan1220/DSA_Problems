be3aefa866c075ad94a7a689c741137b84c0abf8
Sayantan <sayantan.das208@gmail.com>
2022-07-21 02:27:29 +0530

    commit

---
+++ Solution/Array/NumberGoodPairs.java
@@ -0,0 +1,21 @@
+package Solution.Array;
+
+public class NumberGoodPairs {
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i<nums.length-1; i++){
+            for (int j = i+1; j<nums.length; j++){
+                if (nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1,2,3,1,1,3 };
+        long result = numIdenticalPairs(arr);
+        System.out.println(result);
+    }
+}
