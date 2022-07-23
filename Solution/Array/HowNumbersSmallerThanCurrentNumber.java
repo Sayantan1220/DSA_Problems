14420ad98d523987dc8185a576c73ff2876d1c61
Sayantan <sayantan.das208@gmail.com>
2022-07-23 13:13:04 +0530

    Update HowNumbersSmallerThanCurrentNumber.java

--- Solution/Array/HowNumbersSmallerThanCurrentNumber.java
+++ Solution/Array/HowNumbersSmallerThanCurrentNumber.java
@@ -1,3 +1,10 @@
+/*
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. 4
+That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+Return the answer in an array.
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+*/
+
 package Solution.Array;
 
 import java.util.Arrays;
