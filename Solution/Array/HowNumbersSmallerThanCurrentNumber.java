78c907bfe585f2b15126b599aeac51ecd97f2118
Sayantan <sayantan.das208@gmail.com>
2022-07-23 13:11:42 +0530

    commit

---
+++ Solution/Array/HowNumbersSmallerThanCurrentNumber.java
@@ -0,0 +1,29 @@
+package Solution.Array;
+
+import java.util.Arrays;
+
+public class HowNumbersSmallerThanCurrentNumber {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            int count = 0;
+            for (int j = 0; j<nums.length; j++){
+                if (i == j){
+                    continue;
+                }
+                if (nums[i] > nums[j]){
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 8,1,2,2,3 };
+        int[] result = smallerNumbersThanCurrent(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
