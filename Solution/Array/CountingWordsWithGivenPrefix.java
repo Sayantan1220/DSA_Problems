d308779b311c759fa470b24e0c6c8eeb6c029d75
Sayantan <sayantan.das208@gmail.com>
2022-07-26 01:51:28 +0530

    Update CountingWordsWithGivenPrefix.java

--- Solution/Array/CountingWordsWithGivenPrefix.java
+++ Solution/Array/CountingWordsWithGivenPrefix.java
@@ -1,3 +1,10 @@
+/*
+You are given an array of strings words and a string pref.
+Return the number of strings in words that contain pref as a prefix.
+A prefix of a string s is any leading contiguous substring of s.
+https://leetcode.com/problems/counting-words-with-a-given-prefix/
+*/
+
 package Solution.Array;
 
 public class CountingWordsWithGivenPrefix {
