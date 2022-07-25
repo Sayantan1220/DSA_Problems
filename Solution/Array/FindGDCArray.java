76dbbf1a0fe1858c12ff56751af05afd7f59f4ec
Sayantan <sayantan.das208@gmail.com>
2022-07-26 01:39:23 +0530

    Update FindGDCArray.java

--- Solution/Array/FindGDCArray.java
+++ Solution/Array/FindGDCArray.java
@@ -1,3 +1,9 @@
+/*
+Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+*/
+
 package Solution.Array;
 
 public class FindGDCArray {
