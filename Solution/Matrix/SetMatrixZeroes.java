f71973e11d9f7e1be7a66ee5b23cff13fd38b70c
Sayantan <sayantan.das208@gmail.com>
2022-07-27 02:14:28 +0530

    commit

---
+++ Solution/Matrix/SetMatrixZeroes.java
@@ -0,0 +1,39 @@
+package Solution.Matrix;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZeroes {
+    public static int[][] setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        Set<Integer> rows = new HashSet<Integer>();
+        Set<Integer> cols = new HashSet<Integer>();
+
+        // Essentially, we mark the rows and columns that are to be made zero
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        // Iterate over the array once again and using the rows and cols sets, update the elements.
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        int[][] result = setZeroes(matrix);
+        System.out.println(Arrays.deepToString(result));
+    }
+}
