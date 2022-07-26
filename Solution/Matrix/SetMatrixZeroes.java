f8c55665d728720ac9f19189194fcce49b1aa9ea
Sayantan <sayantan.das208@gmail.com>
2022-07-27 02:15:27 +0530

    Update SetMatrixZeroes.java

--- Solution/Matrix/SetMatrixZeroes.java
+++ Solution/Matrix/SetMatrixZeroes.java
@@ -1,3 +1,9 @@
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+You must do it in place.
+https://leetcode.com/problems/set-matrix-zeroes/
+*/
+
 package Solution.Matrix;
 
 import java.util.Arrays;
