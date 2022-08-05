e5a8a9e5940d914a8d1cb5f5f033362aaa19b736
Sayantan <sayantan.das208@gmail.com>
2022-08-05 23:15:56 +0530

    Create LongestConsecutiveSequence.java

---
+++ Solution/Array/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+package Solution.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {2,7,4,1,8,1};
+        int res = longestConsecutive(nums);
+        System.out.println(res);
+    }
+}
