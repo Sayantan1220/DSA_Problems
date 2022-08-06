d4016e9e21eb49fd2e6abc41e21564d255089698
Sayantan <sayantan.das208@gmail.com>
2022-08-07 01:09:58 +0530

    Update LongestSubstring.java

--- Solution/String/LongestSubstring.java
+++ Solution/String/LongestSubstring.java
@@ -1,3 +1,8 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+*/
+
 package Solution.String;
 
 import java.util.HashSet;
