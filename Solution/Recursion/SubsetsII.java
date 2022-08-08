b9a0d778bf45c1f6a0bf607bddf7fb3df2704af7
Sayantan <sayantan.das208@gmail.com>
2022-08-09 02:16:54 +0530

    Update SubsetsII.java

--- Solution/Recursion/SubsetsII.java
+++ Solution/Recursion/SubsetsII.java
@@ -33,8 +33,8 @@ public class SubsetsII {
 
     }
 
-    public static void main(String args[]) {
-        int nums[] = {1,2,2};
+    public static void main(String[] args) {
+        int[] nums = {1,2,2};
         List<List<Integer>> res = subsetsWithDup(nums);
         System.out.println(res);
     }
