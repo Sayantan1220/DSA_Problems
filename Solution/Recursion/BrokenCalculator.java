ca5f5e0d3558055684bc42cddee8666c405c5ad9
Sayantan <sayantan.das208@gmail.com>
2022-11-14 21:01:35 +0530

    FizzBuzz

--- Solution/Recursion/BrokenCalculator.java
+++ Solution/Recursion/BrokenCalculator.java
@@ -16,7 +16,7 @@ public class BrokenCalculator {
         return 1 + brokenCalc(startValue, target + 1);
     }
     
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int start = 3;
         int target = 10;
         int res = brokenCalc(start, target);
